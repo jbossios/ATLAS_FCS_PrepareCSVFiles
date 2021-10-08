af40e0e6a0fa81b97af017f46ca62f398d018c56
Jonathan Bossio <jonathan.bossio@cern.ch>
2021-10-08 09:55:44 +0200

    Allow to use all particles simultaneously
    If more than one particle is used, then pdgId is added such that NN can
    learng differently for each particle

--- MakeCSVfiles_NN.py
+++ MakeCSVfiles_NN.py
@@ -8,150 +8,211 @@
 
 #!/usr/bin/env python3
 
-Particle = 'photons'
+Particles = ['pions']
+#Particles = ['photons']
+#Particles = ['electrons']
+#Particles = ['pions','electrons','photons']
 
 # PATH to input ROOT files
 InputPATH = '/eos/user/j/jbossios/FastCaloSim/MicheleInputs/'
 
 # Where to locate output CSV files
-OutputPATH = '/eos/user/j/jbossios/FastCaloSim/MicheleInputsCSV/'
+OutputPATHBase = '/eos/user/j/jbossios/FastCaloSim/MicheleInputsCSV/'
 
 # Name of TTree
 TTreeName = 'rootTree'
+
+# Produce .txt files
+ProduceTXTs = True # if False, will read already produced TXT files
 
 #############################################
 # DO NOT MODIFY (everything below this line)
 #############################################
 
-InputPATH  += '{}/'.format(Particle)
-OutputPATH += '{}/'.format(Particle)
+InputPATHs = { Particle : '{}{}/'.format(InputPATH,Particle) for Particle in Particles }
+if len(Particles) == 1:
+  OutputPATH = OutputPATHBase+'{}/'.format(Particles[0])
+else:
+  OutputPATH = OutputPATHBase+'_and_'.join(Particles)+'/'
 
 import os,sys,csv
 from ROOT import *
 from ROOT import RDataFrame
 import pandas as pd
+from copy import deepcopy as dp
 
-# CSV header
+# Output CSV header
 Layers  = [0,1,2,3,12]
-if Particle == 'pions': Layers += [13,14]
+if 'pions' in Particles: Layers += [13,14]
 header  = ['e_{}'.format(x) for x in Layers]
 header += ['ef_{}'.format(x) for x in Layers]
 header += ['extrapWeight_{}'.format(x) for x in Layers]
 header += ['etrue']
+if len(Particles) > 1:
+  header += ['pdgId']
 
-# Create a single DF per eta bin to then extract Mean and Std. Dev. 
-DFs = dict()
-for File in os.listdir(InputPATH):
-
-  # skip (non-) phiCorrected files for (pions) electrons
-  if Particle == 'pions'     and 'phiCorrected' in File: continue
-  if Particle == 'electrons' and 'phiCorrected' not in File: continue
-  
-  # Read true energy from input filename
-  Energy = File.split('_')[1].split('E')[1]
-
-  # Read eta range from input filename
-  EtaBin = 'eta_{}_{}'.format(File.split('eta_')[1].split('_')[0],File.split('eta_')[1].split('_')[1])
-
-  # Create DF
-  DF          = RDataFrame(TTreeName,InputPATH+File)                        # create RDataFrame from TTree
-  npy         = DF.AsNumpy()                                                # convert RDataFrame to numpy
-  df          = pd.DataFrame(data=npy, columns=[key for key in npy.keys()]) # convert to pandas/DataFrame
-  df['etrue'] = float(Energy)                                               # add new column with true energy
-  if EtaBin not in DFs:
-    DFs[EtaBin] = df
-  else:
-    DFs[EtaBin] = pd.concat([DFs[EtaBin],df],ignore_index=True)
-
-# Take Mean and Std. Dev. values for all features (energy fractions and true energy)
+# Mean and Std. Dev. values for all features (energy fractions and true energy)
 Means   = dict()
 StdDevs = dict()
-for EtaBin,DF in DFs.items():
-  counter = 0
-  for layer in Layers:
-    if counter == 0:
-      DF['totalEnergy'] = DF['e_{}'.format(layer)]
-    else:
-      DF['totalEnergy'] += DF['e_{}'.format(layer)]
-    counter += 1
-  DF = DF[DF['totalEnergy'] > 0]
-  for layer in Layers:
-    DF['ef_{}'.format(layer)] = DF.apply(lambda row: row['e_{}'.format(layer)] / row['totalEnergy'], axis = 1)
-  Means[EtaBin]   = { var : DF[var].mean() for var in header if 'ef_' in var or var == 'etrue' }
-  StdDevs[EtaBin] = { var : DF[var].std()  for var in header if 'ef_' in var or var == 'etrue' }
-  # save them
-  outTxtFileName = OutputPATH+'MeanStdDevEnergyFractions_{}'.format(EtaBin)+'.txt'
-  outTxtFile     = open(outTxtFileName,'w')
-  for key in Means[EtaBin]:
-    outTxtFile.write('{} {} {}\n'.format(key,Means[EtaBin][key],StdDevs[EtaBin][key]))
-  outTxtFile.close()
 
-# Loop over input files
-for File in os.listdir(InputPATH):
+# Create a single DF per eta bin to then extract Mean and Std. Dev.
+DFs = dict()
 
-  # skip (non-) phiCorrected files for (pions) electrons
-  if Particle == 'pions'     and 'phiCorrected' in File:     continue
-  if Particle == 'electrons' and 'phiCorrected' not in File: continue
+if ProduceTXTs:
+  # Create a single DF per eta bin to then extract Mean and Std. Dev.
+  dFs = dict()
 
-  print('INFO: Preparing CSV file for {}'.format(InputPATH+File))
+  # Loop over particles
+  for Particle in Particles:
 
-  # Read true energy from input filename
-  Energy = File.split('_')[1].split('E')[1]
+    for File in os.listdir(InputPATHs[Particle]):
 
-  # Read eta range from input filename
-  EtaBin = 'eta_{}_{}'.format(File.split('eta_')[1].split('_')[0],File.split('eta_')[1].split('_')[1])
+      # skip (non-) phiCorrected files for (pions) electrons
+      if Particle == 'pions'     and 'phiCorrected' in File: continue
+      if Particle == 'electrons' and 'phiCorrected' not in File: continue
 
-  # Get TTree
-  tfile = TFile.Open(InputPATH+File)
-  if not tfile:
-    print('ERROR: {} not found, exiting'.format(InputPATH+File))
-    sys.exit(1)
-  tree  = tfile.Get(TTreeName)
-  if not tree:
-    print('WARNING: {} not found in {}, file will be skipped'.format(TTreeName,InputPATH+File))
-    continue # skip file
+      # Read true energy from input filename
+      Energy = File.split('_')[1].split('E')[1]
 
-  # Open the CSV file
-  outFileName = OutputPATH+File.replace('.root','.csv')
-  outFile     = open(outFileName, 'w')
-  
-  # Create the csv writer
-  writer = csv.writer(outFile)
+      # Read eta range from input filename
+      EtaBin = 'eta_{}_{}'.format(File.split('eta_')[1].split('_')[0],File.split('eta_')[1].split('_')[1])
+      EtaBin = EtaBin.replace('.root','')
 
-  # write the header
-  writer.writerow(header)
+      # Create DF
+      DF          = RDataFrame(TTreeName,InputPATHs[Particle]+File)             # create RDataFrame from TTree
+      npy         = DF.AsNumpy()                                                # convert RDataFrame to numpy
+      df          = pd.DataFrame(data=npy, columns=[key for key in npy.keys()]) # convert to pandas/DataFrame
+      # remove things I don't need
+      Variables   = ['e_{}'.format(x) for x in Layers]
+      Variables  += ['extrapWeight_{}'.format(x) for x in Layers]
+      if Particle == 'photons' or Particle == 'electrons':
+        for layer in ['13','14']:
+          Variables.remove('e_{}'.format(layer))
+          Variables.remove('extrapWeight_{}'.format(layer))
+      df          = df[Variables]
+      # add new column with true energy
+      df['etrue'] = float(Energy)
+      # add dummy data
+      if Particle == 'electrons' or Particle == 'photons':
+        df.insert(5, 'e_13',            0.0)
+        df.insert(6, 'e_14',            0.0)
+        df.insert(12, 'extrapWeight_13', 0.0)
+        df.insert(13, 'extrapWeight_14', 0.0)
+      if EtaBin not in DFs:
+        DFs[EtaBin] = df
+      else:
+        DFs[EtaBin] = pd.concat([DFs[EtaBin],df],ignore_index=True)
 
-  maxEvents = -1
+  # Take Mean and Std. Dev. values for all features (energy fractions and true energy)
+  for EtaBin,DF in DFs.items():
+    counter = 0
+    for layer in Layers:
+      if counter == 0:
+        DF['totalEnergy'] = DF['e_{}'.format(layer)]
+      else:
+        DF['totalEnergy'] += DF['e_{}'.format(layer)]
+      counter += 1
+    DF = DF[DF['totalEnergy'] > 0]
+    for layer in Layers:
+      DF['ef_{}'.format(layer)] = DF.apply(lambda row: row['e_{}'.format(layer)] / row['totalEnergy'], axis = 1)
+    Means[EtaBin]   = { var : DF[var].mean() for var in header if 'ef_' in var or var == 'etrue' }
+    StdDevs[EtaBin] = { var : DF[var].std()  for var in header if 'ef_' in var or var == 'etrue' }
+    # save them
+    outTxtFileName = OutputPATH+'MeanStdDevEnergyFractions_{}'.format(EtaBin)+'.txt'
+    outTxtFile     = open(outTxtFileName,'w')
+    for key in Means[EtaBin]:
+      outTxtFile.write('{} {} {}\n'.format(key,Means[EtaBin][key],StdDevs[EtaBin][key]))
+    outTxtFile.close()
+else: # read TXTs
+  for File in os.listdir(OutputPATH):
+    if '.txt' not in File: continue
+    if '.swp' in File: continue
+    EtaBin = 'eta'+File.split('_eta')[1].replace('.txt','')
+    Means[EtaBin]   = dict()
+    StdDevs[EtaBin] = dict()
+    txtFile = open(OutputPATH+File)
+    if not txtFile:
+      print('ERROR: {} not found ,exiting'.format(OutputPATH+File))
+      sys.exit(1)
+    for line in txtFile:
+      Values               = line.split(' ')
+      key                  = Values[0].replace('\n','')
+      Means[EtaBin][key]   = float(Values[1].replace('\n',''))
+      StdDevs[EtaBin][key] = float(Values[2].replace('\n',''))
 
-  # Temporary fix due to problem with input TTree for E2097152
-  if 'E2097152' in File:
-    maxEvents = 2000
- 
-  # Loop over events
-  counter = 0
-  for event in tree: 
-    counter += 1
-    if maxEvents != -1:
-      if counter > maxEvents: break
-    # write a row to the csv file
-    totalEnergy = 0 # total deposited energy
-    for var in header:
-      if 'e_' in var:
-        totalEnergy += getattr(tree,var)
-    # write fraction of energy deposited on each layer
-    row = []
-    for var in header:
-      if 'e_' in var:
-        row.append(getattr(tree,var))
-      elif 'ef_' in var:
-        energyFraction = ((getattr(tree,var.replace('ef','e'))/totalEnergy)-Means[EtaBin][var])/StdDevs[EtaBin][var] if totalEnergy!=0 and StdDevs[EtaBin][var]!=0 else 0
-        row.append(energyFraction)
-    row += [getattr(tree,var) for var in header if 'extrapWeight_' in var]         # write extrapolation weight on each layer
-    row.append( (float(Energy)-Means[EtaBin]['etrue']) / StdDevs[EtaBin]['etrue']) # write truth particle's energy
-    writer.writerow(row)
-  
-  # Close the files
-  outFile.close()
-  tfile.Close()
+# Loop over particles
+for Particle in Particles:
+  # Loop over input files
+  for File in os.listdir(InputPATHs[Particle]):
+
+    # skip (non-) phiCorrected files for (pions) electrons
+    if Particle == 'pions'     and 'phiCorrected' in File:     continue
+    if Particle == 'electrons' and 'phiCorrected' not in File: continue
+
+    print('INFO: Preparing CSV file for {}'.format(InputPATHs[Particle]+File))
+
+    # Read true energy from input filename
+    Energy = File.split('_')[1].split('E')[1]
+
+    # Read eta range from input filename
+    EtaBin = 'eta_{}_{}'.format(File.split('eta_')[1].split('_')[0],File.split('eta_')[1].split('_')[1])
+    EtaBin = EtaBin.replace('.root','')
+
+    # Get TTree
+    tfile = TFile.Open(InputPATHs[Particle]+File)
+    if not tfile:
+      print('ERROR: {} not found, exiting'.format(InputPATHs[Particle]+File))
+      sys.exit(1)
+    tree  = tfile.Get(TTreeName)
+    if not tree:
+      print('WARNING: {} not found in {}, file will be skipped'.format(TTreeName,InputPATHs[Particle]+File))
+      continue # skip file
+
+    # Open the CSV file
+    outFileName = OutputPATH+File.replace('.root','.csv')
+    outFile     = open(outFileName, 'w')
+
+    # Create the csv writer
+    writer = csv.writer(outFile)
+
+    # write the header
+    writer.writerow(header)
+
+    maxEvents = -1
+
+    # Temporary fix due to problem with input TTree for E2097152
+    if 'E2097152' in File:
+      maxEvents = 2000
+
+    # Loop over events
+    counter = 0
+    for event in tree:
+      counter += 1
+      if maxEvents != -1:
+        if counter > maxEvents: break
+      # write a row to the csv file
+      totalEnergy = 0 # total deposited energy
+      for var in header:
+        if 'e_' in var:
+          totalEnergy += getattr(tree,var,0.0)
+      # write fraction of energy deposited on each layer
+      row = []
+      for var in header:
+        if 'e_' in var:
+          row.append(getattr(tree,var,0.0))
+        elif 'ef_' in var:
+          energyFraction = ((getattr(tree,var.replace('ef','e'),0.0)/totalEnergy)-Means[EtaBin][var])/StdDevs[EtaBin][var] if totalEnergy!=0 and StdDevs[EtaBin][var]!=0 else 0
+          row.append(energyFraction)
+      row += [getattr(tree,var,0.0) for var in header if 'extrapWeight_' in var]     # write extrapolation weight on each layer
+      row.append( (float(Energy)-Means[EtaBin]['etrue']) / StdDevs[EtaBin]['etrue']) # write truth particle's energy
+      # write pdgid using one hot encoding
+      if len(Particles) > 1:
+        pdgid = {'electrons':0,'photons':1,'pions':2}[Particle]
+        row.append(pdgid) # write pdgid
+      writer.writerow(row)
+
+    # Close the files
+    outFile.close()
+    tfile.Close()
 
 print('>>> ALL DONE <<<')
